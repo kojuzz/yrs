80b05fdb0124f5b8cae035a23ce796fc14e921af
Ko Juzz <sawdani@gmail.com>
2025-05-12 12:18:08 +0700

    [✨ 🚧 62] Return API with Resource Method and User Portal Change Password

--- app/Http/Controllers/Api/UserPortal/ProfileController.php
+++ app/Http/Controllers/Api/UserPortal/ProfileController.php
@@ -3,13 +3,37 @@
 namespace App\Http\Controllers\Api\UserPortal;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserPortal\ProfileResource;
+use App\Services\ResponseService;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
     public function profile()
     {
-        return Auth::guard('users_api')->user();
+        $auth_user = Auth::guard('users_api')->user();
+        return ResponseService::success(new ProfileResource($auth_user));
+    }
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required|string|min:8|max:20',
+            'new_password' => 'required|string|min:8|max:20',
+        ]);
+        try {
+            $auth_user = Auth::guard('users_api')->user();
+            if (!Hash::check($request->old_password, $auth_user->password)) {
+                throw new Exception('The old password is wrong.');
+            }
+            $auth_user->update([
+                'password' => Hash::make($request->new_password)
+            ]);
+            return ResponseService::success([], 'Successfully changed password');
+        } catch (Exception $e) {
+            return ResponseService::fail($e->getMessage());
+        }
     }
 }
---
+++ app/Http/Resources/UserPortal/ProfileResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\UserPortal;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'wallet_balence' => number_format($this->wallet->balance). ' MMK',
+        ];
+    }
+}
